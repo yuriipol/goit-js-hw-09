f4f380bbc08cf0bd7445fabff60494849a89000d
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-12 15:02:52 +0000

    Deploying to gh-pages from @ yuriipol/goit-js-hw-09@f95371f72faca99a3c8ea1257d4d9094683ac28e 🚀

--- 01-color-switcher.335ecad3.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("");
-//# sourceMappingURL=01-color-switcher.335ecad3.js.map
--- 01-color-switcher.fc25d9da.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("");
-//# sourceMappingURL=01-color-switcher.fc25d9da.js.map
