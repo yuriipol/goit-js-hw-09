b4e5be5cade59cda91851818a6a1eb1b3f00ac3d
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-12 19:40:46 +0300

    add coments

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,27 @@
+//зполучаем ссылки на необходимые элементы
 const startChangeColorBtn = document.querySelector('[data-start]');
 const stopChangeColorBtn = document.querySelector('[data-stop]');
 const bodyChangeColor = window.document.body;
 
-let timerId = null;
-
+let timerId = null; //обьявляем переменную и присваиваем ей начальное значение null
+//функция, которая рандомно берет цвета
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+//вешаем события
 startChangeColorBtn.addEventListener('click', onClickChangeColor);
 stopChangeColorBtn.addEventListener('click', onClickStopChangeColor);
-
+//описываем функции
 function onClickChangeColor() {
   timerId = setInterval(() => {
     bodyChangeColor.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  startChangeColorBtn.disabled = true;
-  stopChangeColorBtn.disabled = false;
+  startChangeColorBtn.disabled = true; //делаем start активной
+  stopChangeColorBtn.disabled = false; //делаем stop неактивной
 }
 
 function onClickStopChangeColor() {
-  clearInterval(timerId);
-  startChangeColorBtn.disabled = false;
-  stopChangeColorBtn.disabled = true;
+  clearInterval(timerId); //убираем вызов функции через интервал
+  startChangeColorBtn.disabled = false; //делаем start неактивной
+  stopChangeColorBtn.disabled = true; //делаем stop активной
 }
