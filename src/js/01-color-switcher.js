f95371f72faca99a3c8ea1257d4d9094683ac28e
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-12 18:02:26 +0300

    finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-const startChangeColorBtn = document.querySelector('');
+const startChangeColorBtn = document.querySelector('[data-start]');
+const stopChangeColorBtn = document.querySelector('[data-stop]');
+const bodyChangeColor = window.document.body;
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startChangeColorBtn.addEventListener('click', onClickChangeColor);
+stopChangeColorBtn.addEventListener('click', onClickStopChangeColor);
+
+function onClickChangeColor() {
+  timerId = setInterval(() => {
+    bodyChangeColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startChangeColorBtn.disabled = true;
+}
+
+function onClickStopChangeColor() {
+  clearInterval(timerId);
+  startChangeColorBtn.disabled = false;
+}
