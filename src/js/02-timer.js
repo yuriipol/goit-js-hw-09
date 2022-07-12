054ea708ad6ab72d01ba3eb043ee8392b33e04e9
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-12 20:12:33 +0300

    add library

---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+// Описан в документации
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+require('flatpickr/dist/themes/material_green.css');
+flatpickr('#datetime-picker', options);
