bf8b23a664c0c9d1239f519d5e5fee4a1ae7c771
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-13 23:04:21 +0300

    add task 3 with problems((

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const inputDelay = form.querySelector('.delay');
+const inputAmount = form.querySelector('.amount');
+const inputStep = form.querySelector('.step');
+
+let delay = inputDelay.value;
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  let count = 0;
+
+  const intervalId = setInterval(function () {
+    count += 1;
+    if (count === inputAmount.value) {
+      clearInterval(intervalId);
+    }
+
+    let position = count;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }, (delay += inputStep.value));
+
+  event.currentTarget.reset();
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('Это resolve');
+      } else {
+        reject('Это reject');
+      }
+    }, delay);
+  });
 }
