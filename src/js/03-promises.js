bbcdeeae933f159eb0bebf16ddddf6d60954f202
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-14 16:54:18 +0300

    finish all tasks

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,18 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.form');
-const inputDelay = form.querySelector('.delay');
-const inputAmount = form.querySelector('.amount');
-const inputStep = form.querySelector('.step');
-
-let delay = Number(inputDelay.value);
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
+  //деструктуризация для доступа к элементам формы
   const {
     elements: { delay, step, amount },
   } = event.currentTarget;
+
   let promiseDelay = Number(delay.value);
-  // let count = 0;
 
-  // const intervalId = setInterval(function () {
-  //   count += 1;
-  //   console.log(Number(inputAmount.value));
-  //   if (count === Number(inputAmount.value)) {
-  //     clearInterval(intervalId);
-  //     return;
-  //   }
-
-  //   let position = count;
-
-  //   createPromise(position, delay)
-  //     .then(({ position, delay }) => {
-  //       Notiflix.Notify.success(
-  //         `✅ Fulfilled promise ${position} in ${delay}ms`
-  //       );
-  //     })
-  //     .catch(({ position, delay }) => {
-  //       Notiflix.Notify.failure(
-  //         `❌ Rejected promise ${position} in ${delay}ms`
-  //       );
-  //     });
-  // }, (delay += Number(inputStep.value)));
   for (let i = 1; i <= Number(amount.value); i += 1) {
     createPromise(i, promiseDelay)
       .then(({ position, delay }) => {
